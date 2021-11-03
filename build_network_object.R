faa8f1ea6bed15afb6f472fbf6863629711e450e
tdrexler1 <42881144+tdrexler1@users.noreply.github.com>
2021-11-03 12:33:29 -0500

    advancing chord diagram details: colors, labels, sectors

--- build_network_object.R
+++ build_network_object.R
@@ -48,45 +48,95 @@ county_grouping <-
 
 scls_flow_edges_avg20 <- scls_flow_edges[scls_flow_edges$daily_average>=20.0, ]
 
+
+
+# chordDiagram(
+#   scls_flow_edges_avg20[ ,c(1,2,4)], 
+#   directional = T, 
+#   direction.type = c("diffHeight", "arrows"),
+#   link.arr.type = "big.arrow",
+#   link.sort = TRUE,
+#   link.decreasing = TRUE,
+#   group = county_grouping
+#   )
+
+circos.par(track.margin = c(0.01, 0.01) )
+
+sector_colors = c(
+  WAU = "#4E79A7", 
+  VER = "#F28E28", 
+  SUN = "#59A14F", 
+  STO = "#B6992D", 
+  ORE = "#499894", 
+  MTH = "#E15759", 
+  MOO = "#79706E", 
+  MID = "#D37295", 
+  MCF = "#B07AA1", 
+  FCH = "#9D7660")
+
 chordDiagram(
-  scls_flow_edges_avg20[ ,c(1,2,4)], 
+  scls_flow_edges_avg20[ , c(1,2,4)],
+  grid.col = sector_colors,
+  annotationTrack = c('grid'),
+  annotationTrackHeight = 0.05,
+  #preAllocateTracks = list(track.height = max( strwidth( sector_codes_df$sector_codes) ) ), 
+  #preAllocateTracks = 1,
+  preAllocateTracks = list(list(track.height = mm_h(4) ), list(track.height = mm_h(10)) ),
   directional = T, 
   direction.type = c("diffHeight", "arrows"),
   link.arr.type = "big.arrow",
+  link.sort = TRUE,
+  link.decreasing = TRUE,
   group = county_grouping
+  
   )
 
 
 
+circos.track(track.index = 2, panel.fun = function(x, y) {
+  circos.text(
+    CELL_META$xcenter, 
+    CELL_META$ylim[1], 
+    CELL_META$sector.index, 
+    facing = 'clockwise', 
+    niceFacing = T, adj = c(0, 0.5)
+    )
+  }, bg.border = NA
+  )
+
+highlight.sector(
+  c('DFT', 'FCH', 'MCF', 'MID', 'MOO', 'MTH', 'ORE', 'STO', 'SUN', 'VER', 'WAU'),
+  track.index = 1,
+  col = "#F6AE2D",
+  text = 'DANE COUNTY',
+  text.col = '#000000',
+  facing = 'bending.outside',
+  niceFacing = T
+)
+
+circos.info()
+circos.clear()
 
 
 
+for(si in get.all.sector.index()){
+  circos.axis(
+    h = "bottom",
+    sector.index = si,
+    track.index = 1,
+    labels = F,
+    major.tick = F
+  )
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# TODO: change sector/link colors
+# TODO: label county groupings
+# TODO: change order of sectors w/in groups
+# TODO: orient labels w/ spacing; horizontal/vertical?
+# TODO: add plot title, caption
+# TODO: add interactivity with Shiny? tootips on hover would be really helpful
 
 
 
