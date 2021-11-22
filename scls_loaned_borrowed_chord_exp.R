df2f55c772fc5f4ab0eb844bd13c547ded44832f
tdrexler1 <42881144+tdrexler1@users.noreply.github.com>
2021-11-22 13:01:46 -0600

    sapply working to select counties w/ total spans > 20 degrees

--- scls_loaned_borrowed_chord_exp.R
+++ scls_loaned_borrowed_chord_exp.R
@@ -166,51 +166,113 @@ circos.track(
 
 # Outer Sectors: County Names --------------------------------------------------
 #sapply
-total_county_deg = c()
+# max(
+#   sapply(
+#     names(which(county_grouping=="Dane")), function(library_02){
+#       if(get.cell.meta.data("cell.start.degree", library_02)!=0){
+#         get.cell.meta.data("cell.start.degree", library_02)
+#       }else{
+#         360.0
+#       }
+#     }
+#   )
+# )-min(
+#   sapply(
+#     names(which(county_grouping=="Dane")), function(library_02){
+#       get.cell.meta.data("cell.end.degree", library_02)
+#     }
+#   )
+# )
 
-for (county in unique(county_grouping)){
-  print(county)
-  county_libraries = c()
-  county_start_deg = c()
-  county_end_deg = c()
-  
-  #print(get.cell.meta.data("cell.end.degree", names(which(county_grouping==county))))
-  for (library in names(which(county_grouping==county))){
-    #print(get.cell.meta.data("cell.end.degree", library))
-    county_libraries <- c(county_libraries, library)
-    if(get.cell.meta.data("cell.start.degree", library)!=0){
-      county_start_deg <- c(county_start_deg, get.cell.meta.data("cell.start.degree", library))
-    }else{
-      county_start_deg <- c(county_start_deg, 360.0)
-    }
-    county_end_deg <- c(county_end_deg, get.cell.meta.data("cell.end.degree", library))
-  }
-  print(county_libraries)
-  print(county_start_deg)
-  print(county_end_deg)
-  
-  print(
-    paste(
-      "Start:", max(county_start_deg), 
-      " End:", min(county_end_deg),
-      " Size:", max(county_start_deg)-min(county_end_deg)
+county_spans <- 
+  sapply(unique(county_grouping), function(county){
+      max(sapply(names(which(county_grouping==county)), function(library){
+            if(get.cell.meta.data("cell.start.degree", library)!=0){
+              get.cell.meta.data("cell.start.degree", library)
+            }else{
+              360.0
+            }
+          })
+      )-min(sapply(names(which(county_grouping==county)), function(library){
+            get.cell.meta.data("cell.end.degree", library)
+          })
       )
-    )
-  #print(min(county_start_deg))
-  total_county_deg <- c(total_county_deg, max(county_start_deg)-min(county_end_deg))
-}
-total_county_deg <- 
-  structure(
-    total_county_deg,
-    names=unique(county_grouping)
+    }
   )
 
-total_county_deg
-highlight_counties <- names(which(total_county_deg > 20))
-highlight_counties
+# total_county_deg_02 <- 
+#   sapply(
+#     unique(county_grouping), function(county_02){
+#       max(
+#         sapply(
+#           names(which(county_grouping==county_02)), function(library_02){
+#             if(get.cell.meta.data("cell.start.degree", library_02)!=0){
+#               get.cell.meta.data("cell.start.degree", library_02)
+#             }else{
+#               360.0
+#             }
+#           }
+#         )
+#       )-min(
+#         sapply(
+#           names(which(county_grouping==county_02)), function(library_02){
+#             get.cell.meta.data("cell.end.degree", library_02)
+#           }
+#         )
+#       )
+#     }
+#   )
+
+# ---------------------------- this all works --------------------------------
+# total_county_deg = c()
+# 
+# for (county in unique(county_grouping)){
+#   print(county)
+#   county_libraries = c()
+#   county_start_deg = c()
+#   county_end_deg = c()
+#   
+#   #print(get.cell.meta.data("cell.end.degree", names(which(county_grouping==county))))
+#   for (library in names(which(county_grouping==county))){
+#     #print(get.cell.meta.data("cell.end.degree", library))
+#     county_libraries <- c(county_libraries, library)
+#     if(get.cell.meta.data("cell.start.degree", library)!=0){
+#       county_start_deg <- c(county_start_deg, get.cell.meta.data("cell.start.degree", library))
+#     }else{
+#       county_start_deg <- c(county_start_deg, 360.0)
+#     }
+#     county_end_deg <- c(county_end_deg, get.cell.meta.data("cell.end.degree", library))
+#   }
+#   print(county_libraries)
+#   print(county_start_deg)
+#   print(county_end_deg)
+#   
+#   print(
+#     paste(
+#       "Start:", max(county_start_deg), 
+#       " End:", min(county_end_deg),
+#       " Size:", max(county_start_deg)-min(county_end_deg)
+#       )
+#     )
+#   #print(min(county_start_deg))
+#   total_county_deg <- c(total_county_deg, max(county_start_deg)-min(county_end_deg))
+# }
+# total_county_deg <- 
+#   structure(
+#     total_county_deg,
+#     names=unique(county_grouping)
+#   )
+# 
+# total_county_deg
+# highlight_counties <- names(which(county_spans > 20))
+# highlight_counties
+
+# -----------------------------------------------------------------^
 
 # highlight counties with enough libraries to fit county name on outer sector
-highlight_counties <- names(which(table(county_grouping) > 5))
+highlight_counties <- names(which(county_spans > 20))
+highlight_counties
+#highlight_counties <- names(which(table(county_grouping) > 5))
 
 # all other libraries
 other_sect <- names(county_grouping[which(!county_grouping %in% highlight_counties)])
